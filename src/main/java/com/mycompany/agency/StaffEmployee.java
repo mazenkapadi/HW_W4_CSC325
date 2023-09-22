747529d5d27edd7f5113e36521667178d1b96c62
Alyssa Cowen <123402425+alyssacowen@users.noreply.github.com>
2023-09-22 08:56:24 -0400

    Updated pay

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -35,6 +35,6 @@ public class StaffEmployee extends StaffMember
     //-----------------------------------------------------------------
     public double pay()
     {
-       
+       return rate;
     }
 }
