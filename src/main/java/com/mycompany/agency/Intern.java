0f310b46321008a2d82c2f2676d4634ebcd39cbb
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-09-22 21:15:40 -0400

    Bug fixed

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -4,7 +4,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern /*extends StaffEmployee*/ {
+public class Intern extends StaffEmployee {
 
 
     //-----------------------------------------------------------------
@@ -12,17 +12,17 @@ public class Intern /*extends StaffEmployee*/ {
     //  information.
     //-----------------------------------------------------------------
     public Intern(String eName, String eAddress, String ePhone) {
-        /*super(eName,eAddress,ePhone,"XXX", 0);
+        super(eName,eAddress,ePhone,"XXX", 0);
 
         name = eName;
         address = eAddress;
-        phone = ePhone;*/
+        phone = ePhone;
     }
 
     //-----------------------------------------------------------------
     // TODO 3:  Returns a zero pay value for this intern.
     //-----------------------------------------------------------------
     public double pay() {
-        //return payRate = 0.0;
+        return payRate = 0.0;
     }
 }
